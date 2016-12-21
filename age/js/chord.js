d3a64bc46fb18b3e0dc160d4082d1bfbfcd8154e
arihant parsoya <parsoyaarihant@gmail.com>
2016-12-21 18:38:14 +0530

    Removed year-label and added second title

--- age/js/chord.js
+++ age/js/chord.js
@@ -98,21 +98,6 @@ label.append("foreignObject")
         return "<div class='color-labels-box' style = 'background: #e41a1c'> </div> Under 20 <br /><br /> <div class='color-labels-box' style = 'background: #377eb8'> </div> 20-24 <br /> <br /><div class='color-labels-box' style = 'background: #4daf4a'> </div> 25-29 <br /><br /><div class='color-labels-box' style = 'background: #984ea3'> </div> 30-34 <br /><br /><div class='color-labels-box' style = 'background: #ff7f00'> </div> 35-39 <br /><br /><div class='color-labels-box' style = 'background: #DAC237'> </div> 40-44 <br /><br /><div class='color-labels-box' style = 'background: #a65628'> </div> 45-49 <br /><br /><div class='color-labels-box' style = 'background: #f781bf'> </div> 50 and Above <br /><br />";
     });
 
-label.append("foreignObject")
-    .attr("width", 100)
-    .attr("height", 100)
-    .attr("y", 10)
-    .attr("x", width / 2 - 50)
-    .append("xhtml:body")
-    .style("fill", "#000")
-    .style("text-align", "center")
-    .style("font-size", "20px")
-    .attr("class", "noselect")
-    .attr("id", "year-label")
-    .html(function() {
-        return "<p>1984</p>";
-    });
-
 d3.csv("data/colors.csv", function(rel) {
     d3.json("data/matrix.json", function(matrix) {
         dataset = matrix;
@@ -131,7 +116,6 @@ d3.csv("data/colors.csv", function(rel) {
         stepSlider.noUiSlider.on('update', function(values, handle) {
             slider_value = parseInt(values);
             updateChords(dataset[parseInt(values)]);
-            $("#year-label").html(slider_value + 1984);
         });
     });
 });
--- religion/js/chord.js
+++ religion/js/chord.js
@@ -98,21 +98,6 @@ label.append("foreignObject")
         return "<div class='color-labels-box' style = 'background: #e41a1c'> </div> Christianity <br /><br /> <div class='color-labels-box' style = 'background: #377eb8'> </div> Buddhism <br /> <br /><div class='color-labels-box' style = 'background: #4daf4a'> </div> Taoism<br /> <br><div class='color-labels-box' style = 'background: #984ea3'> </div> Hinduism<br /> <br><div class='color-labels-box' style = 'background: #ff7f00'> </div> Other Religion<br /> <br><div class='color-labels-box' style = 'background: #DAC237'> </div> Non Religious<br /> <br>";
     });
 
-label.append("foreignObject")
-    .attr("width", 100)
-    .attr("height", 100)
-    .attr("y", 10)
-    .attr("x", width / 2 - 50)
-    .append("xhtml:body")
-    .style("fill", "#000")
-    .style("text-align", "center")
-    .style("font-size", "20px")
-    .attr("class", "noselect")
-    .attr("id", "year-label")
-    .html(function() {
-        return "<p>1984</p>";
-    });
-
 d3.csv("data/colors.csv", function(rel) {
     d3.json("data/matrix.json", function(matrix) {
         dataset = matrix;
@@ -131,7 +116,6 @@ d3.csv("data/colors.csv", function(rel) {
         stepSlider.noUiSlider.on('update', function(values, handle) {
             slider_value = parseInt(values);
             updateChords(dataset[parseInt(values)]);
-            $("#year-label").html(slider_value + 1984);
         });
     });
 });
