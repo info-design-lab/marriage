d49284b5a598812437bddf5c32b439127bcbe3bd
arihant parsoya <parsoyaarihant@gmail.com>
2016-12-21 23:17:37 +0530

    manas changes

--- education/js/chord.js
+++ education/js/chord.js
@@ -1,8 +1,8 @@
 
 var width = 900,
-    height = 620,
-    outerRadius = Math.min(width, height) / 2 - 100,
-    innerRadius = outerRadius - 18;
+height = 620,
+outerRadius = Math.min(width, height) / 2 - 100,
+innerRadius = outerRadius - 18;
 var dataset;
 var duration = 200;
 var formatPercent = d3.format("%");
@@ -19,84 +19,84 @@ var last_layout;
 var neighborhoods;
 
 function getDefaultLayout() {
-    return d3.layout.chord()
-        .padding(0.03)
-        .sortSubgroups(d3.descending)
-        .sortChords(d3.ascending);
+    return customChordLayout()
+    .padding(0.03)
+
+    .sortChords(d3.ascending);
 }
 
 var arc = d3.svg.arc()
-    .innerRadius(innerRadius)
-    .outerRadius(outerRadius);
+.innerRadius(innerRadius)
+.outerRadius(outerRadius);
 
 var path = d3.svg.chord()
-    .radius(innerRadius);
+.radius(innerRadius);
 
 var chart = d3.select("#chart_placeholder").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+.attr("width", width)
+.attr("height", height);
 
 var label = chart.append('g');
 
 var g = chart
-    .append("g")
-    .attr("id", "circle")
-    .attr("transform",
-        "translate(" + width / 2 + "," + height / 2 + ")");
+.append("g")
+.attr("id", "circle")
+.attr("transform",
+    "translate(" + width / 2 + "," + height / 2 + ")");
 
 g.append("circle")
-    .attr("r", outerRadius)
-    .attr("id", "rotate-reference");
+.attr("r", outerRadius)
+.attr("id", "rotate-reference");
 
 g.append("text")
-    .attr("y", -outerRadius * 1.1)
-    .attr("x", outerRadius * 0.2)
-    .attr("class", "noselect")
-    .text(function(d) {
-        return '\uf221'
-    })
-    .style("fill", "#b2b2b2")
-    .attr('font-family', 'FontAwesome')
-    .attr('font-size', function(d) {
-        return "50px"
-    });
+.attr("y", -outerRadius * 1.1)
+.attr("x", outerRadius * 0.2)
+.attr("class", "noselect")
+.text(function(d) {
+    return '\uf221'
+})
+.style("fill", "#b2b2b2")
+.attr('font-family', 'FontAwesome')
+.attr('font-size', function(d) {
+    return "50px"
+});
 
 g.append("text")
-    .attr("y", -outerRadius * 1.1)
-    .attr("x", -outerRadius * 0.4)
-    .attr("class", "noselect")
-    .text(function(d) {
-        return '\uf222'
-    })
-    .style("fill", "#b2b2b2")
-    .attr('font-family', 'FontAwesome')
-    .attr('font-size', function(d) {
-        return "43px"
-    });
+.attr("y", -outerRadius * 1.1)
+.attr("x", -outerRadius * 0.4)
+.attr("class", "noselect")
+.text(function(d) {
+    return '\uf222'
+})
+.style("fill", "#b2b2b2")
+.attr('font-family', 'FontAwesome')
+.attr('font-size', function(d) {
+    return "43px"
+});
 
 g.append("line")
-    .attr("x1", -4)
-    .attr("y1", -outerRadius * 1.3)
-    .attr("x2", 4)
-    .attr("y2", outerRadius * 1.3)
-    .style("stroke-dasharray", ("3, 3"))
-    .style("stroke", "#b2b2b2")
-    .style("stroke-width", 2)
-    .style("opacity", 0.6);
+.attr("x1", -4)
+.attr("y1", -outerRadius * 1.3)
+.attr("x2", 4)
+.attr("y2", outerRadius * 1.3)
+.style("stroke-dasharray", ("3, 3"))
+.style("stroke", "#b2b2b2")
+.style("stroke-width", 2)
+.style("opacity", 0.6);
 
 
 label.append("foreignObject")
-    .attr("width", 400)
-    .attr("height", 200)
-    .attr("y", 400)
-    .attr("x", 100)
-    .append("xhtml:body")
-    .style("fill", "#b2b2b2")
-    .attr("class", "noselect")
-    .attr("class", "color-labels")
-    .html(function() {
-        return "<div class='color-labels-box' style = 'background: #377eb8'> </div> Secondary & Below <br /><br /> <div class='color-labels-box' style = 'background: #4daf4a'> </div> Post-secondary <br /> <br /><div class='color-labels-box' style = 'background: #f781bf'> </div> University<br />";
-    });
+.attr("width", 400)
+.attr("height", 200)
+.attr("y", 400)
+.attr("x", 100)
+.append("xhtml:body")
+.style("fill", "#b2b2b2")
+.attr("class", "noselect")
+.attr("class", "color-labels")
+.html(function() {
+    return "<div class='color-labels-box' style = 'background: #377eb8'> </div> Secondary & Below <br /><br /> <div class='color-labels-box' style = 'background: #4daf4a'> </div> Post-secondary <br /> <br /><div class='color-labels-box' style = 'background: #f781bf'> </div> University<br />";
+});
 
 d3.csv("data/colors.csv", function(rel) {
     d3.json("data/matrix.json", function(matrix) {
@@ -104,18 +104,23 @@ d3.csv("data/colors.csv", function(rel) {
         neighborhoods = rel;
         updateChords(dataset[0]);
 
-          noUiSlider.create(stepSlider, {
+        noUiSlider.create(stepSlider, {
             start: [0],
             step: 1,
             range: {
-                'min': [0],
-                'max': [31]
+                'min': [1984],
+                'max': [2015]
             },
-            });
+            pips: {
+                mode: 'values',
+                values: [1984,1990,1995,2000,2005,2010,2015],
+                density: 4
+            }
+        });
 
         stepSlider.noUiSlider.on('update', function(values, handle) {
             slider_value = parseInt(values);
-            updateChords(dataset[parseInt(values)]);
+            updateChords(dataset[slider_value-1984]);
         });
 
 
@@ -129,80 +134,80 @@ function updateChords(matrix) {
     layout.matrix(matrix);
 
     var groupG = g.selectAll("g.group")
-        .data(layout.groups(), function(d) {
-            return d.index;
-        });
+    .data(layout.groups(), function(d) {
+        return d.index;
+    });
 
     groupG.exit()
-        .transition()
-        .duration(duration)
-        .attr("opacity", 0)
-        .remove();
+    .transition()
+    .duration(duration)
+    .attr("opacity", 0)
+    .remove();
 
     var newGroups = groupG.enter().append("g")
-        .attr("class", "group");
+    .attr("class", "group");
 
     newGroups.append("title");
 
     groupG.select("title")
-        .text(function(d, i) {
-            return numberWithCommas(d.value) +
-                " " +
-                neighborhoods[i].name;
-        });
+    .text(function(d, i) {
+        return numberWithCommas(d.value) +
+        " " +
+        neighborhoods[i].name;
+    });
 
     newGroups.append("path")
-        .attr("id", function(d) {
-            return "group" + d.index;
-        })
-        .style("fill", function(d) {
-            return neighborhoods[d.index].color;
-        });
+    .attr("id", function(d) {
+        return "group" + d.index;
+    })
+    .style("fill", function(d) {
+        return neighborhoods[d.index].color;
+    });
 
     groupG.select("path")
-        .transition()
-        .duration(duration)
-        .attr("opacity", 0.55)
-        .attrTween("d", arcTween(last_layout));
+    .transition()
+    .duration(duration)
+    .attr("opacity", 0.55)
+    .attrTween("d", arcTween(last_layout));
 
     var chordPaths = g.selectAll("path.chord")
-        .data(layout.chords(), chordKey);
+    .data(layout.chords(), chordKey);
 
     var newChords = chordPaths.enter()
-        .append("path")
-        .attr("class", "chord");
+    .append("path")
+    .attr("class", "chord");
 
     newChords.append("title");
     chordPaths.select("title")
-        .text(function(d) {
-            if (neighborhoods[d.target.index].name !== neighborhoods[d.source.index].name) {
+    .text(function(d) {
+        if (neighborhoods[d.target.index].name !== neighborhoods[d.source.index].name) {
                 //console.log(d.source.value);
                 return [numberWithCommas(d.source.value),
-                    " ",
-                    neighborhoods[d.source.index].name,
-                    " married ",
-                    neighborhoods[d.target.index].name,
+                " ",
+                neighborhoods[d.source.index].name,
+                " married ",
+                neighborhoods[d.target.index].name,
                 ].join("");
             } else {
                 return numberWithCommas(d.source.value) +
-                    " started and ended in " +
-                    neighborhoods[d.source.index].name;
+                " started and ended in " +
+                neighborhoods[d.source.index].name;
             }
         });
 
     chordPaths.exit().transition()
-        .duration(duration)
-        .attr("opacity", 0)
-        .remove();
+    .duration(duration)
+    .attr("opacity", 0)
+    .remove();
 
     chordPaths.transition()
-        .duration(duration)
-        .attr("opacity", 0.5)
-        .style("fill", function(d) {
-            return neighborhoods[d.source.index].color;
-        })
-        .attrTween("d", chordTween(last_layout))
-        .transition().duration(duration).attr("opacity", 0.5);
+    .duration(duration)
+    .attr("opacity", 0.5)
+    .style("fill", function(d) {
+        return neighborhoods[d.source.index].color;
+    })
+    .attrTween("d", chordTween(last_layout))
+    .transition().duration(duration).attr("opacity", 0.5);
 
     groupG.on("mouseover", function(d) {
         chordPaths.classed("fade", function(p) {
@@ -246,8 +251,8 @@ function arcTween(oldLayout) {
 
 function chordKey(data) {
     return (data.source.index < data.target.index) ?
-        data.source.index + "-" + data.target.index :
-        data.target.index + "-" + data.source.index;
+    data.source.index + "-" + data.target.index :
+    data.target.index + "-" + data.source.index;
 }
 
 function chordTween(oldLayout) {
@@ -302,16 +307,16 @@ function chordTween(oldLayout) {
 
 function mycode() {
     // do some stuff...
-    if (slider_value <=31 && playing == 1 && !firstrun) {
+    if (slider_value <=2015 && playing == 1 && !firstrun) {
         stepSlider.noUiSlider.set(slider_value);
         slider_value = slider_value + 1;
         document.getElementById("play").src = "images/pause.png";
-    } else if (slider_value == 31 && playing!=2) {
+    } else if (slider_value == 2016) {
         playing = 2;
         document.getElementById("play").src = "images/reset.png";
     }
-    else if (slider_value < 31 && playing ==2 && !firstrun) {
-        document.getElementById("play").src = "images/pause.png";
+    else if (slider_value <= 2015 && playing ==2 && !firstrun) {
+        document.getElementById("play").src = "images/play.png";
         playing=0;
     }
     tid = setTimeout(mycode, duration);
@@ -352,9 +357,9 @@ Array.prototype.contains = function(obj) {
 }
 
 d3.select("#chart_placeholder")
-    .on("mousemove", mousemove)
-    .on("mouseup", mouseup)
-    .on("mousedown", mousedown);
+.on("mousemove", mousemove)
+.on("mouseup", mouseup)
+.on("mousedown", mousedown);
 
 function mouse(e) {
     if (e != undefined) {
@@ -389,8 +394,8 @@ function mouseup(e) {
     if(mouse_rotate){
       old_angle = dm + old_angle;
       mouse_rotate = false;
-    }
-    $('html,body').css('cursor', 'default');
+  }
+  $('html,body').css('cursor', 'default');
 }
 
 function mousedown(e) {
