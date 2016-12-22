f7085fd5198e331b9bcf68ef16cab8c57b17c26a
Manas Sharma <sharma.manas271196@gmail.com>
2016-12-22 18:09:46 +0530

    Add files via upload

--- education/js/line.js
+++ education/js/line.js
@@ -19,6 +19,36 @@ q.defer(Educationlinechart,5);
 q.await(function(error) {
 });
 
+d3.select("body")
+.style("position","relative")
+.insert("g",":nth-child(3)")
+.style("position","absolute")
+.attr("width", 100)
+.attr("height", 110)
+.style("top", 700)
+.style("left", 50)
+.append("foreignObject",":nth-child(2)")
+.attr("width", 100)
+.attr("height", 110)
+.append("xhtml:body")
+.style("fill", "#b2b2b2")
+.attr("class", "noselect")
+.attr("class", "color-labels")
+.html(function() {
+  return "<svg width='10' height='10'> \
+        <rect width='10' height='10' style='fill:#377eb8;opacity: 0.5;' /> \
+      </svg> \
+        Secondary & Below  <br>\
+      <svg width='10' height='10'> \
+        <rect width='10' height='10' style='fill:#4daf4a;opacity: 0.5;' /> \
+      </svg> \
+        Post-secondary <br>\
+      <svg width='10' height='10'> \
+        <rect width='10' height='10' style='fill:#f781bf;opacity: 0.5;' /> \
+      </svg> \
+        University <br>";
+});
+
 var margin = {top: 20, right: 65, bottom: 20, left: 50},
 width2 = 325 - margin.left - margin.right,
 height2 = 200 - margin.top - margin.bottom;
